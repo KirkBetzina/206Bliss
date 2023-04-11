0a46b27e169e334309e5a7ae4ceb307dc43f6555
kirk betzina <kirk.betzina@gmail.com>
2023-04-10 20:50:28 -0400

    bulding the nav

--- 206bliss/src/components/Nav.js
+++ 206bliss/src/components/Nav.js
@@ -1,8 +1,18 @@
-const Form = () =>{
+const Nav = () =>{
     return(
         <div id = 'Nav'>
-            <a href = 'AboutMe' />
-            <a href = 'Form'></a>
+            <ul>
+                <li a href='aboutme'>About Me</li>
+                <li a href='Sign In'>Sign In</li>
+                <li a href='Book a Trip'>Book A Trip</li>
+                <li a href='Take a Travel Survey'>Take a Travel Survey</li>
+                <li a href='Testimonials'>Testimonials</li>
+                <li a href='Home'>Home</li>
+                <li a href='Services'>Services</li>
+                <li a href='blogAndPodcast'>Blog and Podcast</li>
+                
+
+            </ul>
         </div>
     )
 }
